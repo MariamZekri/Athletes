f1e2a5246c408c5d8be68d150e6dadb9bcb4b002
mariam abdelhamid <mariam.abdelhamid@v-apps.co>
2017-10-19 12:32:52 +0200

    print

--- Athletes/ViewController.swift
+++ Athletes/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        
+        print("test")
         
         
         // Do any additional setup after loading the view, typically from a nib.
